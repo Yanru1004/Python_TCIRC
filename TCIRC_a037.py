e56045dd3bec59f1e5df499f06f3232f666ef2e4
Yanru1004 <devil1004@livemail.tw>
2026-04-29 23:19:22 +0800

    2026029

--- TCIRC_a037.py
+++ TCIRC_a037.py
@@ -1,1 +1,15 @@
-#a037 我也愛偶數
+#a037 我也愛偶數 (swap 版)
+
+#讀取a,b
+a,b = list(map(int,input().split(' ')))
+
+if a > b:
+    a,b = b,a
+
+result = 0
+
+for i in range(a,b+1):
+    if i%2 == 0:
+        result += i
+
+print(result)
--- TCIRC_a039.py
+++ TCIRC_a039.py
@@ -1,1 +1,12 @@
 #a039 小明的作業
+
+#讀取三邊長並排序。
+
+score = 0
+
+for i in range(5):
+    a,b,c = sorted(list(map(int,input().split(' '))))
+    
+    if a+ b > c:
+        score += 1
+print(score)
