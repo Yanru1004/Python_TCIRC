5b91058afbebae7faffc2118fd5aa39b00783fb2
Yanru1004 <devil1004@livemail.tw>
2026-06-02 23:52:58 +0800

    20260602

--- TCIRC_b014.py
+++ TCIRC_b014.py
@@ -1,1 +1,23 @@
 #b014 串串的三角形
+
+#讀取三邊長
+
+a,b,c = map(int,input().split(' '))
+
+x = a**2 + b**2
+y = c**2
+
+if a + b <= c:
+    print('NULL')
+
+elif x > y:
+    print('Acute Triangle')
+
+elif x == y:
+    print('Right Triangle') 
+
+elif x < y:
+    print('Obtuse Triangle')
+
+else:
+    print('Error')
--- TCIRC_b015.py
+++ TCIRC_b015.py
@@ -1,1 +1,46 @@
 #b015 數字的顏色
+
+while True:
+
+    
+    try:
+        
+        #讀取數字
+
+        num = int(input())
+        color = 'White'
+
+
+
+        if num % 2 == 0:
+            color = 'Red'
+
+        if num % 3 == 0:
+            if color == 'White':
+                color = 'Yellow'
+
+            elif color == 'Red':
+                color = 'Orange'
+
+        if num % 5 == 0:
+            color = 'Black'
+            
+
+        if num % 7 == 0:
+            if color == 'White':
+                color = 'Blue'
+            
+            elif color == 'Red':
+                color = 'Purple'
+            
+            elif color == 'Yellow':
+                color = 'Green'
+            
+            elif color == 'Orange' or color == 'Black':
+                color = 'Black'
+        
+        print(color)    
+        
+
+    except:
+        break
