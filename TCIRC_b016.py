6f7566a1772b580836620b8ac246c021d8c0b7bf
Yanru1004 <devil1004@livemail.tw>
2026-06-03 23:23:49 +0800

    20260603

--- TCIRC_b016.py
+++ TCIRC_b016.py
@@ -1,1 +1,8 @@
 #b016 山谷的回聲
+
+#讀取次數
+
+m = int(input())
+
+for i in range(m):
+    print('Hello, World!')
--- TCIRC_b017.py
+++ TCIRC_b017.py
@@ -1,1 +1,12 @@
 #b017 病毒的增生
+
+#讀取增生次數
+
+m = int(input())
+
+num = 1
+
+for i in range(m):
+    num *= 2
+
+print(num)
