4afdeae5c6e4f0f2781d60fdf18873073b0644fd
Yanru1004 <devil1004@livemail.tw>
2026-06-11 23:28:15 +0800

    20260611

--- TCIRC_b026.py
+++ TCIRC_b026.py
@@ -1,1 +1,8 @@
 #b026 乖乖寫作業
+
+#讀入答案
+
+_,*ans = input().split()
+
+print(' '.join(ans[::-1]))
+
--- TCIRC_b027.py
+++ TCIRC_b027.py
@@ -1,1 +1,15 @@
 #b027 找出學霸
+
+n,*score = map(int,input().split())
+
+result = [-1,-1,-1,101]
+
+for i in range(n):
+    if score[i] > result[1]:
+        result[:2] = i+1,score[i]
+    
+    if score[i] < result[3]:
+        result[2:] = i+1,score[i]
+
+
+print(' '.join(map(str,result)))
