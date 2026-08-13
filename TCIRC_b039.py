236a89dc8b1c490ea32dfb95be5e9ad8365d1d38
Yanru1004 <devil1004@livemail.tw>
2026-08-13 23:22:56 +0800

    Update TCIRC_b039.py

--- TCIRC_b039.py
+++ TCIRC_b039.py
@@ -1,1 +1,21 @@
-#b039 Stack
+#b039 Stack 模板題
+import sys
+input = sys.stdin.readline
+
+num = int(input())
+
+stack = []
+result = []
+
+for i in range(num):
+    command= input().split()
+
+    if command[0] == "PUSH":
+        stack.append(command[1])
+
+    elif command[0] == "POP":
+        if len(stack):
+            result.append(stack.pop())
+
+print('\n'.join(result))
+
