f0e6dc2b7c14a265828657d767d6318825eb7317
Yanru1004 <devil1004@livemail.tw>
2026-08-14 23:14:03 +0800

    Update TCIRC_b041.py

--- TCIRC_b041.py
+++ TCIRC_b041.py
@@ -1,1 +1,27 @@
 #b041 括弧配對
+
+def check(s:str):
+    box = []
+    pair_dict = {'>':'<',
+                 ')':'(',
+                 ']':'[',
+                 '}':'{'}
+    
+    for sy in s:
+        if sy in ['<','(','[','{']:
+            box.append(sy)
+        elif box != [] and box[-1] == pair_dict[sy]:
+            box.pop()
+        else:
+            return 'F'
+    return 'T' if box == [] else 'F'
+
+result = []
+while True:
+    try:
+        s = input()
+        result.append(check(s))
+    except:
+        break
+
+print('\n'.join(result))
