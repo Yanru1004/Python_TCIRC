ee497292ae4e58507ea993ada1f1532fb188864e
Yanru1004 <devil1004@livemail.tw>
2026-08-15 23:07:58 +0800

    Update TCIRC_b043.py

--- TCIRC_b043.py
+++ TCIRC_b043.py
@@ -1,1 +1,18 @@
 #b043 Queue
+
+import queue
+
+box = queue.Queue()
+
+num = int(input())
+result =[]
+
+for i in range(num):
+    command = input().split()
+    if command[0] == 'PUSH':
+        box.put(command[1])
+    elif not box.empty() and command[0] == 'POP':
+        result.append(box.get())
+        box.task_done
+        
+print('\n'.join(result))
