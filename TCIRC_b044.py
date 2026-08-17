742ee6c1f3814c1b56d461f510e63d6026258492
Yanru1004 <devil1004@livemail.tw>
2026-08-17 23:25:40 +0800

    Update TCIRC_b044.py

--- TCIRC_b044.py
+++ TCIRC_b044.py
@@ -1,1 +1,18 @@
 #b044 卑鄙約瑟夫
+
+n,k = map(int,input().split())
+
+party = [x for x in range(n)]
+ptr = 1
+
+while len(party) > 1:
+    new_party = []
+    for p in party:
+        if ptr < k:
+            new_party.append(p)
+            ptr += 1
+        else:
+            ptr = 1
+    party = new_party
+
+print(party[0])
