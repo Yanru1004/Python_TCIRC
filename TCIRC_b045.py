8055b10bf31d761d2f25aee198bb1d5c38f09bbf
Yanru1004 <devil1004@livemail.tw>
2026-08-20 23:49:45 +0800

    Update TCIRC_b045.py

--- TCIRC_b045.py
+++ TCIRC_b045.py
@@ -1,1 +1,21 @@
-#b045 Priority
+#b045 Priority Queue 模板題
+
+import heapq
+
+num = int(input())
+
+h = []
+result = []
+
+for i in range(num):
+    command = input().split()
+
+    if command[0] == 'PUSH':
+        heapq.heappush(h,int(command[1])*-1)
+
+    else:
+        if len(h) != 0:
+          result.append(h[0]*-1)
+          heapq.heappop(h)
+
+print('\n'.join(map(str,result)))
