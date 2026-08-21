b2ab867c483bfcfe403834cc8da4d434d25ba143
Yanru1004 <devil1004@livemail.tw>
2026-08-21 23:31:17 +0800

    Update TCIRC_b046.py

--- TCIRC_b046.py
+++ TCIRC_b046.py
@@ -1,1 +1,18 @@
 #b046 加法的成本
+
+import heapq
+
+_ = int(input())
+result = 0
+h = [int(x) for x in input().split()]
+heapq.heapify(h)
+
+while len(h) >= 2:
+    num1 = heapq.heappop(h)
+    num2 = heapq.heappop(h)
+    cost = num1 + num2
+    result += cost
+    heapq.heappush(h,cost)
+
+print(result)
+
