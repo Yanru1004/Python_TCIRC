634c8ef684bacfd218f765fea724cd29d43afe9f
Yanru1004 <devil1004@livemail.tw>
2026-08-28 23:34:57 +0800

    Update TCIRC_b048.py

--- TCIRC_b048.py
+++ TCIRC_b048.py
@@ -1,1 +1,29 @@
-#b048 基礎
+#b048 y=a+b
+
+n,y = map(int,input().split())
+
+li = [int(x) for x in input().split()]
+
+head,tail = 0,len(li)-1
+have_sol = False
+
+while True:
+
+    calc = li[head] + li[tail]
+
+    if calc == y:
+        have_sol = True
+        break
+    elif calc > y:
+        tail -= 1
+
+    elif calc < y:
+        head += 1
+
+    if head == tail:
+        print("IMPOSSIBLE")
+        break
+
+if have_sol:
+    print(head,tail)
+    
