43a4664d5578febb2264125dc63b98fa016369d6
Yanru1004 <devil1004@livemail.tw>
2026-08-29 23:08:36 +0800

    Update TCIRC_b052.py

--- TCIRC_b052.py
+++ TCIRC_b052.py
@@ -1,1 +1,17 @@
 #b052 減法遊戲
+
+_ = input()
+
+li = [int(x) for x in input().split()]
+
+max_num = li[0]
+pre_result = li[0]
+
+for i in range(1,len(li)):
+
+    new_result = max(pre_result - li[i],li[i])
+    pre_result = (new_result)
+    if pre_result > max_num:
+        max_num = pre_result
+
+print(max_num)
